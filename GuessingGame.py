0d7d71f81a5cba63519aa7541b7563b7b67bed92
mahdiabdessalem <115706932+mahdiabdessalem@users.noreply.github.com>
2024-04-04 10:36:26 +0200

    GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,24 +1,24 @@
-
-
 import random
 
 if __name__ == "__main__":
 
-    min_val = 0
-    max_val = 100
-    computer_num = random.randint(min_val, max_val)
-    user_input = None
+  min_val = 0
+  max_val = 100
+  computer_num = random.randint(min_val, max_val)
+  user_input = None
+  num_guesses = 0  # Initialize a variable to count attempts
 
-    while user_input != computer_num:
-        try:
-            user_input = int(input(f"Guess a number between {min_val} and {max_val}: "))
+  while user_input != computer_num:
+    try:
+      user_input = int(input(f"Guess a number between {min_val} and {max_val}: "))
+      num_guesses += 1  # Increment attempts on each guess
 
-            if user_input > computer_num:
-                print("Too Big")
-            elif user_input < computer_num:
-                print("Too Small")
-        except Exception:
-            print('ERROR: Please provide a number not any characters')
-            user_input = None
+      if user_input > computer_num:
+        print("Too Big")
+      elif user_input < computer_num:
+        print("Too Small")
+    except Exception:
+      print('ERROR: Please provide a number not any characters')
+      user_input = None
 
-    print(f"YOU DID IT! {computer_num} was the correct number!")
+  print(f"YOU DID IT! {computer_num} was the correct number in {num_guesses} attempts!")
